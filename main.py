35b9411c8c20b3f226bdcfe6e8769d0ef8de488f
ignpoppyseed <87223326+ignpoppyseed@users.noreply.github.com>
2022-03-09 17:56:45 -0800

    Added features
    - WikiHow Search
    - Possum image generator

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import discord
+import asyncio
+import random
 from discord.ext import commands
 from bottoken import TOKEN
+from images import possumIMG
 
 # Initialize Bot and Denote The Command Prefix
-bot = commands.Bot(command_prefix=">")
+bot = commands.Bot(case_insensitive=True, command_prefix=">",activity= discord.Activity(type=discord.ActivityType.watching, name="poppy test catgirl"), status=discord.Status.dnd)
 bot.remove_command('help')
+
 
 # Runs when Bot Succesfully Connects
 @bot.event
@@ -20,6 +25,8 @@ async def on_message(message):
         await message.channel.send(f'hello {message.author}!')
     if message.content == 'do you love me catgirl':
         await message.channel.send(f'No, {message.author} no one does.')
+    if message.content.startswith('im'):
+        await message.channel.send(f'Hi{message.content[2:]}, im dad.')
 
     await bot.process_commands(message)
 
@@ -42,5 +49,14 @@ async def multi(ctx, arg1, arg2): # The name of the function is the name of the 
 @bot.command()
 async def wiki(ctx, wiki): # The name of the function is the name of the command
     await ctx.send('https://www.wikihow.com/wikiHowTo?search='+(str(wiki))) # ctx.send sends text in chat
+
+possumImage=["https://cdn.discordapp.com/attachments/946618549428969512/951281697226362891/IMG_9335.jpg", 
+"https://cdn.discordapp.com/attachments/946618549428969512/946619221830418493/IMG_9036.jpg", 
+"https://cdn.discordapp.com/attachments/946618549428969512/946619222119841862/IMG_9037.jpg"]
+
+@bot.command()
+async def possum(ctx):
+    numImg = random.randrange(0, 21)
+    await ctx.send(possumIMG[numImg])
 
 bot.run(TOKEN)
